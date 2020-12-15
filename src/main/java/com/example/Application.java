e9ca89c22db9c83c572eefb997e5d3ac2c143ee2
Sultan Baig <sultanbaig93@gmail.com>
2020-12-15 11:46:39 +0530

    modifying dependency and adding servlet intializer

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,15 +4,22 @@ package com.example;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import com.example.entity.Employee;
 import com.example.repository.EmployeeRepository;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    	return builder.sources(Application.class);
     }
 
     @Bean
